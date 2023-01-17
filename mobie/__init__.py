439e1a50e435fabaa1d5b15a0f9b34e89f66a028
Christian Tischer <christian.tischer@embl.de>
2023-01-17 17:32:09 +0100

    Improve versioning

--- mobie/__init__.py
+++ mobie/__init__.py
@@ -4,17 +4,19 @@ import jgo.util
 import os
 import pathlib
 import sys
+from importlib.metadata import version
+
 
 _picocli_autocomplete    = 'picocli.AutoComplete'
 _mobie_main_class        = 'org.embl.mobie.cmd.MoBIECommandLineInterface'
 _groupId                 = 'org.embl.mobie'
 _artifactId              = 'mobie-viewer-fiji'
+_artifactVersion         = version(mobie)
 
 def launch_mobie():
     return jgo.util.main_from_endpoint(
         argv=sys.argv[1:],
         primary_endpoint=f'{_groupId}:{_artifactId}',
-        primary_endpoint_main_class=_mobie_main_class)
-
-if __name__ == '__main__':
-    launch_mobie()
+        primary_endpoint_main_class=_mobie_main_class,
+        primary_endpoint_version=_artifactVersion
+)
